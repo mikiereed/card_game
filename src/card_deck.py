226bf96e9ced372d7500f046b8f294ad18d19151
mikie Reed <mikie@prenotions.com>
2021-01-14 13:13:29 -0800

    add shuffle_cards, fix sort_cards

--- src/card_deck.py
+++ src/card_deck.py
@@ -1,4 +1,5 @@
 from operator import attrgetter
+import random
 from src.card import Card
 
 CARD_VALUE_STRINGS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
@@ -10,6 +11,8 @@ class CardDeck():
         for suit in CARD_SUITS:
             for value in CARD_VALUE_STRINGS:
                 self.cards.append((suit, value))
+        if shuffled == True:
+            self.shuffle_cards()
 
     def get_card_count(self):
         return len(self.cards)
@@ -17,9 +20,18 @@ class CardDeck():
     def pull_card_from_deck(self):
         return self.cards.pop()
 
+    def shuffle_cards(self):
+        shuffled_cards = []
+        while self.cards:
+            random_number = random.randint(0, self.get_card_count() - 1)
+            random_card = self.cards.pop(random_number)
+            shuffled_cards.append(random_card)
+        self.cards = shuffled_cards.copy()
+
     def sort_cards(self, sort_order = ['Spades', 'Hearts', 'Diamonds', 'Clubs']):
-        sort_order_values = {key: i for i, key in enumerate(sort_order)}
-        self.cards.sort(key=lambda x: sort_order_values[x[0]])
+        sort_order_suits = {key: i for i, key in enumerate(sort_order)}
+        sort_order_values = {key: i for i, key in enumerate(CARD_VALUE_STRINGS)}
+        self.cards.sort(key=lambda x: (sort_order_suits[x[0]], sort_order_values[x[1]]))
 
     def __repr__(self):
         card_count = self.get_card_count()
@@ -28,15 +40,4 @@ class CardDeck():
         else:
             plural = 's'
         return f'Deck with {card_count} Card{plural}'
-
-deck = CardDeck()
-# deck.sort_cards(['Spades', 'Clubs', 'Diamonds', 'Hearts'])
-# for card in deck.cards:
-#     print(card)
-card = deck.pull_card_from_deck()
-print(card)
-card = deck.pull_card_from_deck()
-print(card)
-card = deck.pull_card_from_deck()
-print(card)
-print(deck)
+        
--- tests/test_card_deck.py
+++ tests/test_card_deck.py
@@ -12,24 +12,27 @@ def test_pull_card_from_deck():
     is_pulled_from_deck = (card_deck.get_card_count() == 51)
     assert is_right_type and is_pulled_from_deck
 
+def test_shuffle_cards():
+    card_deck = CardDeck(shuffled=False)
+    unshuffled_cards_sample = card_deck.cards[:5]
+    card_deck.shuffle_cards()
+    shuffled_cards_sample = card_deck.cards[:5]
+    assert unshuffled_cards_sample != shuffled_cards_sample
+
+
 class TestSortCards():
     
     def test_sort_cards_default(self):
-        card_deck = CardDeck()
+        card_deck = CardDeck(shuffled=True)
         card_deck.sort_cards()
-        sorted_suits = self.get_suits_from_sort(card_deck.cards)
-        expected = ['Spades', 'Hearts', 'Diamonds', 'Clubs'] # default from CardDeck.sort_cards() method
-        assert sorted_suits == expected
+        sorted_cards_sample = card_deck.cards[::13]
+        # default suits order from CardDeck.sort_cards() method
+        expected = [('Spades', '2'), ('Hearts', '2'), ('Diamonds', '2'), ('Clubs', '2')]
+        assert sorted_cards_sample == expected
 
     def test_sort_cards(self):
-        card_deck = CardDeck()
+        card_deck = CardDeck(shuffled=True)
         card_deck.sort_cards(['Clubs', 'Hearts', 'Spades', 'Diamonds'])
-        sorted_suits = self.get_suits_from_sort(card_deck.cards)
-        expected = ['Clubs', 'Hearts', 'Spades', 'Diamonds']
-        assert sorted_suits == expected
-
-    def get_suits_from_sort(self, deck):
-        """Return the suits for the 1st, 14th, 27th, 40th cards"""
-        suits = [suit for suit, value in deck]
-        suits = suits[0::13]
-        return suits
+        sorted_cards_sample = card_deck.cards[::13]
+        expected = [('Clubs', '2'), ('Hearts', '2'), ('Spades', '2'), ('Diamonds', '2')]
+        assert sorted_cards_sample == expected
