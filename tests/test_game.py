1f6877e7a8360b35a03d90dbdd1c81ad9b7ec230
mikie Reed <mikie@prenotions.com>
2021-01-15 14:36:41 -0800

    split test_deal_and_print_cards

--- tests/test_game.py
+++ tests/test_game.py
@@ -9,8 +9,8 @@ This package is called and run through pytest.
 from src.card_deck import CardDeck
 import src.game as game
 
-def test_deal_and_print_cards():
-    """Tests that _deal_and_print_cards() function works properly."""
+def test_deal_and_print_cards_amount_of_cards():
+    """Tests that _deal_and_print_cards() gives each player the right amount of cards."""
     card_deck = CardDeck()
     _, players_cards = game._setup_players_and_card_lists()
     game._deal_and_print_cards(card_deck, players_cards, column_width=0)
@@ -18,13 +18,19 @@ def test_deal_and_print_cards():
     cards_per_player = len(players_cards[0])
     expected_cards_per_player = game.CARDS
 
+    assert cards_per_player == expected_cards_per_player
+
+def test_deal_and_print_cards_removing_from_deck():
+    """Tests that _deal_and_print_cards() removes cards from the deck."""
+    card_deck = CardDeck()
+    _, players_cards = game._setup_players_and_card_lists()
+    game._deal_and_print_cards(card_deck, players_cards, column_width=0)
+
     remaining_cards = card_deck.get_card_count()
     standard_deck_count = 52
     expected_remaining_cards = standard_deck_count - (game.CARDS * game.PLAYERS)
 
-    is_correct_number_of_cards = (cards_per_player == expected_cards_per_player)
-    is_correct_cards_remaining = (remaining_cards == expected_remaining_cards)
-    assert is_correct_number_of_cards and is_correct_cards_remaining
+    assert remaining_cards == expected_remaining_cards
 
 def test_get_and_print_scores():
     """Tests that _get_and_print_scores() function works properly."""
